c01d3bd2f27944e0ea9aceb5eb3194f519a5e4cf
Alia TETO <91055704+Tetoalia@users.noreply.github.com>
2022-03-07 11:43:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const randomilize = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  const randomizeColor = (lightness = 0.3) => {
+    return `hsl(${randomilize(0, 360)},${randomilize(0, 100)}%,${randomilize(
+      0,
+      100 * lightness
+    )}%)`;
+  };
+  const nameElement = document.getElementById("owner-name");
+  nameElement.addEventListener("click", () => {
+    const randomColor = randomizeColor();
+    document.body.style.setProperty("--primary-color", randomColor);
+  });
